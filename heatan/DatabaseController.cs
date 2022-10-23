10728101350d57c12037045c7006728abb358892
okita <junor01@gmail.com>
2022-10-23 21:42:31 +0900

    init

---
+++ heatan/DatabaseController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+public class DatabaseController
+    {
+    public string SQL { set;  get; }
+
+    public SqlConnection Connection { set;  get; }
+
+    public DatabaseController()
+    {
+        OpenConnection();
+    }
+
+    protected Boolean OpenConnection()
+    {        
+        string currentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+        XElement xml = XElement.Load(currentDirectory + "DatabaseConnection.xml");
+
+        try
+        {
+            var cs = xml.Element("ConnectString").Value;
+            Connection = new SqlConnection(cs);
+            Connection.Open();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            //MessageBox.Show(ex.Message, "通知");
+            return false;
+        }
+    }
+
+    public string VersionInfo()
+    {
+        SqlCommand command = new SqlCommand();
+        {
+            // バージョン情報取得SQLを実行します。
+            command.Connection = Connection;
+            command.CommandText = "select version()";
+            var value = command.ExecuteScalar();
+            var versionNo = value.ToString();
+            return ($"{versionNo}");
+        }
+
+    }
+
+    public DataTable ReadAsDataTable()
+    {
+        using (SqlCommand command = new SqlCommand(SQL, Connection))
+        {
+            DataTable dt;
+            var addapter = new SqlDataAdapter(command);
+            dt = new DataTable();
+            addapter.Fill(dt);
+            return dt;
+        }
+    }
+    public SqlDataReader ReadAsDataReader()
+    {
+        using (SqlCommand command = new SqlCommand(SQL, Connection)) 
+        {
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                return reader;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+
+    public bool ExecuteProcedure()
+    {
+        using (SqlCommand command = new SqlCommand(SQL, Connection))
+        {
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = command.ExecuteReader();
+            return (reader != null);
+        }
+    }
+
+
+
+}
---
+++ heatan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heatan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dc = new DatabaseController();
+            using (SqlCommand command = new SqlCommand("usp_insert_room_status", dc.Connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@arg_sense_time", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                command.Parameters.AddWithValue("@arg_temperature", 12.34);
+                command.Parameters.AddWithValue("@arg_heating_status", 1);
+
+                command.Parameters.Add("ReturnValue", SqlDbType.Int);
+                command.Parameters["ReturnValue"].Direction = ParameterDirection.ReturnValue;
+
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+    }
+}
